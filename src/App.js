f88389fe3634ec2af739c9f9410aaeb4e66cba7a
Abdu <abdulrahman.omar@student.moringaschool.com>
2023-04-28 20:14:40 +0300

    scaffolds create app

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import Connect from './Components/LandingPage/Connect';
+import Footer from './Components/LandingPage/Footer';
+import Footersection from './Components/LandingPage/Footersection';
+import Jobs from './Components/LandingPage/Jobs';
+import Join from './Components/LandingPage/Join';
+import NavPanel from './Components/LandingPage/NavPanel';
+import Post from './Components/LandingPage/Post';
+import Testimonals from './Components/LandingPage/Testimonals';
+import Topics from './Components/LandingPage/Topics';
+import Welcome from './Components/LandingPage/Welcome';
+// import {BrowserRouter ,  Routes , Route} from "react-router-dom"
+import Maincontent from './Components/Main Page/Maincontent';
+import Login from './Components/LoginPage/Login';
+
+
+
+
+
+function App() {
+  return(
+    <div className="App">
+   
+        <NavPanel />
+      <Welcome />
+      <Topics />
+      <Jobs />
+      <Post />
+      <Testimonals />
+      <Connect /> 
+      <Join />
+       <Footer />
+      <Footersection />
+{/*    
+      <Maincontent /> */}
+
+      {/* <Login /> */}
+      
+      
+   </div>
+     
+  );
+}
+
+export default App;
+// npm install @mui/material @emotion/react @emotion/styled
+//npm install @mui/icons-material
+
